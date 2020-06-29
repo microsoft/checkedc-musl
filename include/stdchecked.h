3616b5b17a4b2361bedbf8b319d1895c2a37fbc7
dopelsunce <sosccc1@gmail.com>
2020-06-29 17:09:04 -0500

    add stdchecked.h

---
+++ include/stdchecked.h
@@ -0,0 +1,17 @@
+#ifndef __STDCHECKED_H
+#define __STDCHECKED_H
+
+#define ptr _Ptr
+#define array_ptr _Array_ptr
+#define nt_array_ptr _Nt_array_ptr
+#define checked _Checked
+#define nt_checked _Nt_checked
+#define unchecked _Unchecked
+#define bounds_only _Bounds_only
+#define where _Where
+#define dynamic_check _Dynamic_check
+#define dynamic_bounds_cast _Dynamic_bounds_cast
+#define assume_bounds_cast _Assume_bounds_cast
+#define return_value _Return_value
+
+#endif /* __STDCHECKED_H */
