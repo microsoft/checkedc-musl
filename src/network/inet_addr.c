316be0f9bdeff2952230ab000fc03ef606c77705
dopelsunce <sosccc1@gmail.com>
2020-07-16 15:32:01 -0500

    Convert functions in arpa/inet.h

--- src/network/inet_addr.c
+++ src/network/inet_addr.c
@@ -1,8 +1,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#pragma CHECKED_SCOPE on
 
-in_addr_t inet_addr(const char *p)
+in_addr_t inet_addr(const char *p : itype(_Nt_array_ptr<const char>))
 {
 	struct in_addr a;
 	if (!__inet_aton(p, &a)) return -1;
--- src/network/inet_ntoa.c
+++ src/network/inet_ntoa.c
@@ -1,10 +1,16 @@
 #include <arpa/inet.h>
 #include <stdio.h>
+#pragma CHECKED_SCOPE on
 
-char *inet_ntoa(struct in_addr in)
+// inet_ntoa allocates and returns a null-terminated buffer of 16 bytes,
+// which converts to an nt_array_ptr with a count of 15.
+char *inet_ntoa(struct in_addr in) : count(15) itype(_Nt_array_ptr<char>)
 {
-	static char buf[16];
-	unsigned char *a = (void *)&in;
-	snprintf(buf, sizeof buf, "%d.%d.%d.%d", a[0], a[1], a[2], a[3]);
+	static char buf _Nt_checked[16];
+	// Struct in_addr has 4 bytes.
+	_Array_ptr<unsigned char> a : count(4) = (_Array_ptr<unsigned char>)&in;
+	_Unchecked {
+		snprintf((char *)buf, sizeof buf, "%d.%d.%d.%d", a[0], a[1], a[2], a[3]);
+	}
 	return buf;
 }
