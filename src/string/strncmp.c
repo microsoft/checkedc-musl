75a9e06cbcf55086fdbea14b1cc19337918f23b8
emoham <t-emoham@microsoft.com>
2020-07-17 13:18:41 -0700

    remove extra spaces

--- src/string/strncmp.c
+++ src/string/strncmp.c
@@ -4,7 +4,7 @@ int strncmp(const char *_l : itype(_Nt_array_ptr<const char>) count(n),
             const char *_r : itype(_Nt_array_ptr<const char>) count(n),
             size_t n)
 _Checked{
-	
+
 	_Nt_array_ptr<const unsigned char> l : count(n) = (_Nt_array_ptr<const unsigned char>)_l;
 	_Nt_array_ptr<const unsigned char> r : count(n) = (_Nt_array_ptr<const unsigned char>)_r;
 	if (!n--) return 0;
