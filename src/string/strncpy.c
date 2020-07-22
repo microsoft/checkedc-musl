c77621309e5ff3adf3fdef7d4ca3660858d40679
emoham <t-emoham@microsoft.com>
2020-07-22 15:16:44 -0700

    Remove extra spaces

--- src/string/strncpy.c
+++ src/string/strncpy.c
@@ -1,8 +1,8 @@
 #include <string.h>
 
 char *strncpy(char *restrict d : itype(restrict _Nt_array_ptr<char>) count(n),
-                     const char *restrict s,
-                     size_t n)
+              const char *restrict s,
+              size_t n)
   : itype(_Nt_array_ptr<char>) count(n)
 {
 	__stpncpy(d, s, n);
