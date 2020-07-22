03f725986e3b589960f8a711107355af6f0689a7
emoham <t-emoham@microsoft.com>
2020-07-22 15:10:52 -0700

    Remove comnments

--- src/string/stpncpy.c
+++ src/string/stpncpy.c
@@ -15,7 +15,6 @@ char *__stpncpy(char *restrict d : itype(restrict _Array_ptr<char>) count(n),
 #ifdef __GNUC__
 	typedef size_t __attribute__((__may_alias__)) word;
 	_Array_ptr<word> wd : count(n) = 0;
-//	_Array_ptr<const word> ws : count(n) = 0;
 	const word *ws;
 	if (((uintptr_t)s & ALIGN) == ((uintptr_t)d & ALIGN)) {
 		for (; ((uintptr_t)s & ALIGN) && n && (*d=*s); n--, s++, d++);
