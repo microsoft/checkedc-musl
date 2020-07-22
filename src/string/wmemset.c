ed3b28674e602ee38d13d876949d254958f14c52
emoham <t-emoham@microsoft.com>
2020-07-21 17:17:36 -0700

    Remove extra space

--- src/string/wmemset.c
+++ src/string/wmemset.c
@@ -2,7 +2,7 @@
 
 wchar_t *wmemset(wchar_t *d : itype(_Nt_array_ptr<wchar_t>) count(n),
                  wchar_t c,
-                 size_t n) 
+                 size_t n)
   : itype(_Nt_array_ptr<wchar_t>) count(n)
 _Checked{
 	_Nt_array_ptr<wchar_t> ret : count(n) = d;
