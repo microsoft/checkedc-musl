0a5a374bd7d07a67f76d17e3a6bc5fda97c60154
dopelsunce <sosccc1@gmail.com>
2020-08-27 12:47:28 -0500

    Revert "revert stpncpy.c"
    This reverts commit 2ea7977239a48b06c2e2c89895b78c5382b3ffb4.

--- src/string/stpncpy.c
+++ src/string/stpncpy.c
@@ -16,16 +16,18 @@ char *__stpncpy(char *restrict d : itype(restrict _Nt_array_ptr<char>) count(n -
 	typedef size_t __attribute__((__may_alias__)) word;
 	_Array_ptr<word> wd : count(n) = 0;
 	const word *ws;
-	if (((uintptr_t)s & ALIGN) == ((uintptr_t)d & ALIGN)) {
-		for (; ((uintptr_t)s & ALIGN) && n && (*d=*s); n--, s++, d++);
-		if (!n || !*s) goto tail;
-		wd=(_Array_ptr<word>)d; ws=(const void *)s;
-		for (; n>=sizeof(size_t) && !HASZERO(*ws);
-		       n-=sizeof(size_t), ws++, wd++) *wd = *ws;
-		d=(restrict _Array_ptr<void>)wd; s=(const void *)ws;
-	}
+	// TODO: fix the compiler errors.
+	// if (((uintptr_t)s & ALIGN) == ((uintptr_t)d & ALIGN)) {
+	// 	for (; ((uintptr_t)s & ALIGN) && n && (*d=*s); n--, s++, d++);
+	// 	if (!n || !*s) goto tail;
+	// 	wd=(_Array_ptr<word>)d; ws=(const void *)s;
+	// 	for (; n>=sizeof(size_t) && !HASZERO(*ws);
+	// 	       n-=sizeof(size_t), ws++, wd++) *wd = *ws;
+	// 	d=(restrict _Array_ptr<void>)wd; s=(const void *)ws;
+	// }
 #endif
-	for (; n && (*d=*s); n--, s++, d++);
+	// TODO: fix the compiler errors.
+	// for (; n && (*d=*s); n--, s++, d++);
 tail:
 	memset(d, 0, n);
 	return d;
