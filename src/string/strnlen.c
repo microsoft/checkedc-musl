ac921fac367894ef54c3d8530271c69201458dcf
Katherine Kjeer <kakje@microsoft.com>
2020-09-15 12:43:16 -0700

    Change strnlen itype to _Nt_array_ptr to match header declaration

--- src/string/strnlen.c
+++ src/string/strnlen.c
@@ -1,6 +1,6 @@
 #include <string.h>
 
-size_t strnlen(const char *s : itype(_Array_ptr<const char>) count(n), size_t n)
+size_t strnlen(const char *s : itype(_Nt_array_ptr<const char>) count(n), size_t n)
 _Checked{
 	_Array_ptr<const char> p : count(n / sizeof(const char)) = memchr(s, 0, n);
 	return p ? p-s : n;
